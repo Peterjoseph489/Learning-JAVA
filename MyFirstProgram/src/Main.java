bbcfd1ff006ef9d721b15f959b9f07ac4fee3a64
Peter Joseph <josephpeter489@gmail.com>
2023-10-26 11:17:50 +0100

    Initial Commit.

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("I love pizza\n");
+		System.out.println("It's really good!");
+		
+		System.out.print("\t'I love pizza'\n");
+		System.out.print("\"I love pizza\"");
+		
+		
+		// This is a comment.
+		
+	}
+ 
+}
