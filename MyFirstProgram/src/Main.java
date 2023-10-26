01aec656ddc8627d2c351f4da2d47e002fa669f2
Peter Joseph <josephpeter489@gmail.com>
2023-10-26 17:44:53 +0100

    Initial Commit.

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -7,11 +7,48 @@ public class Main {
 		System.out.println("I love pizza\n");
 		System.out.println("It's really good!");
 		
-		System.out.print("\t'I love pizza'\n");
+		System.out.print("\t'I love pizza'\n"); 
 		System.out.print("\"I love pizza\"");
+		System.out.println();
+		System.out.      println();
+		
+		
+		int x; //declaration
+		x = 123; //assignment
+		
+		int a = 123; //initialization
+		
+		System.out.println(a);
+		System.out.println("My number is: "+a);
+		
+		
+		long b = 123356780795487L;
+		System.out.println(b);
+		
+		byte c = 100;
+		System.out.println(c);
+		
+		float d = 3.14f;
+		System.out.println(d);
+		
+		double e = 3.14;
+		System.out.println(e);
+		
+		
+		
 		
 		
 		// This is a comment.
+		
+		/*
+		 * This
+		 * is
+		 * a
+		 * comment.
+		 * 
+		 */
+		
+		
 		
 	}
  
