3a13a7e391d1ad24b063449515c53391f4d20910
Peter Joseph <josephpeter489@gmail.com>
2023-10-27 10:27:09 +0100

    scanner object

--- MyFirstProgram/src/MyScanner.java
+++ MyFirstProgram/src/MyScanner.java
@@ -6,5 +6,18 @@ public class MyScanner {
         Scanner scanner = new Scanner(System.in);
 
         // Your code logic here
+        System.out.println("What is your name? ");
+        String name = scanner.nextLine();
+        System.out.println("How old are you? ");
+        int age = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("What is your favourite food?");
+        String food = scanner.nextLine();
+        
+        System.out.println("Hello "+name);
+        System.out.println("You are "+age+" years old");
+        System.out.println("You like "+food);
+        
+        
     }
 }
