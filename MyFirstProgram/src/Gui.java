e5ccbb2eb7e4dde8ee7d6508570ca6322a35601e
Peter Joseph <josephpeter489@gmail.com>
2023-10-27 11:42:52 +0100

    GUI - Project

---
+++ MyFirstProgram/src/Gui.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Gui {
+
+	public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("Enter your name"); 
+        // Your code logic here
+        
+    }
+}
+
