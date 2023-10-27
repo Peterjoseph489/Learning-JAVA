5ddc747ad53c41917819e7fbc0c314f9a2b1b2fc
Peter Joseph <josephpeter489@gmail.com>
2023-10-27 10:34:57 +0100

    expression

---
+++ MyFirstProgram/src/Expression.java
@@ -0,0 +1,15 @@
+
+public class Expression {
+
+	public static void main(String[] args) {
+		
+		
+		// expression = operands & operators
+		// operands = Values, variables, numbers, quantity
+		// operators = + - * / %
+		
+		
+		
+		
+	}
+}
