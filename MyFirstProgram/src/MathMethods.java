4c721a303a9d50b7b1670280dd1cd6581928c4ba
Peter Joseph <josephpeter489@gmail.com>
2023-10-28 14:09:36 +0100

    math methods

--- MyFirstProgram/src/MathMethods.java
+++ MyFirstProgram/src/MathMethods.java
@@ -3,5 +3,28 @@ public class MathMethods {
 
 	public static void main(String[] args) {
 		
+		double x = 3.14;
+		double y = -10;
+		
+		double z = Math.max(x, y);
+		System.out.println(z);
+		
+		double a = Math.min(x, y);
+		System.out.println(a);
+		
+		double b = Math.abs(y);
+		System.out.println(b);
+		
+		double c = Math.sqrt(x);
+		System.out.println(c);
+		
+		double d = Math.round(x);
+		System.out.println(d);
+		
+		double e = Math.ceil(x);
+		System.out.println(e);
+		
+		double f = Math.floor(x);
+		System.out.println(f);
 	}
 }
