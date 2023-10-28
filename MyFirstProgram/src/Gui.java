c2e84d226a93b90ec8b24f40e0109614761cf76e
Peter Joseph <josephpeter489@gmail.com>
2023-10-28 13:37:28 +0100

    continuous learning

--- MyFirstProgram/src/Gui.java
+++ MyFirstProgram/src/Gui.java
@@ -9,6 +9,10 @@ public class Gui {
         
         int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age"));
         JOptionPane.showMessageDialog(null, "You are  "+age+" years old.");
+        
+        
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height"));
+        JOptionPane.showMessageDialog(null, "You are  "+height+" cm tall.");
     }
 }
 
