aa5b3fa26069f64e882fa0e552ecdfe17b6dd66c
Peter Joseph <josephpeter489@gmail.com>
2023-11-06 10:16:16 +0100

    IfStatement

--- MyFirstProgram/src/IfStatement.java
+++ MyFirstProgram/src/IfStatement.java
@@ -4,5 +4,17 @@ public class IfStatement {
 		
 		// If statement = Performs a block of code if it's condition evaluates to be true
 		
+		int age = 19;
+		
+		if (age >= 74) {
+			System.out.println("You are an Old Man!");
+		} else if (age >=18) {
+			System.out.println("You are not an adult!");
+		} else if (age >=13) {
+			System.out.println("You are a teenager!");
+		} 
+		else {
+			System.out.println("You are not an adult!");
+		}
 	}
 }
