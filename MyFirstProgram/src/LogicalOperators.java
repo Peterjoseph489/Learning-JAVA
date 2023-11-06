0cd69deb2396fee24f3346132119b0795955e094
Peter Joseph <josephpeter489@gmail.com>
2023-11-06 12:54:09 +0100

    Logical Operator

--- MyFirstProgram/src/LogicalOperators.java
+++ MyFirstProgram/src/LogicalOperators.java
@@ -23,12 +23,24 @@ public class LogicalOperators {
 		
 		
 		
+//		Scanner scanner = new Scanner(System.in);
+		
+//		System.out.println("You are playing a game! Press q or Q to Quit");
+//		String response = scanner.next();
+//		
+//		if(response.equals("q") || response.equals("Q")) {
+//			System.out.println("You quit the game");
+//		} else {
+//			System.out.println("You are still playing the game *pew pew*");
+//		}
+		
+		
 		Scanner scanner = new Scanner(System.in);
 		
 		System.out.println("You are playing a game! Press q or Q to Quit");
 		String response = scanner.next();
 		
-		if(response.equals("q") || response.equals("Q")) {
+		if(!response.equals("q") && !response.equals("Q")) {
 			System.out.println("You quit the game");
 		} else {
 			System.out.println("You are still playing the game *pew pew*");
