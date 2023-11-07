14e180ec198a847bcbfafeeec37f467567b8cf4e
Peter Joseph <josephpeter489@gmail.com>
2023-11-07 15:28:19 +0100

    Array continues

--- MyFirstProgram/src/Array.java
+++ MyFirstProgram/src/Array.java
@@ -5,6 +5,20 @@ public class Array {
 		
 		// array = used to store multiple values in a single variable
 		
+		
+		
+//		String[] cars =  {"Camaro", "Corvette", "Tesla"};
+//		System.out.println(cars[1]);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		String[] cars = new String[3];
 		
 		cars[0] = "Camaro";
@@ -14,5 +28,7 @@ public class Array {
 		for(int i = 0; i < cars.length; i++) {
 			System.out.println(cars[1]);
 		}
+		
+		
 	}
 }
