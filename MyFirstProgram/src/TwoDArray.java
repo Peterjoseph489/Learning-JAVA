68c702a9b34abaf0aefd189535c6abb3557df390
Peter Joseph <josephpeter489@gmail.com>
2023-11-07 15:41:56 +0100

    2D Array

---
+++ MyFirstProgram/src/TwoDArray.java
@@ -0,0 +1,22 @@
+
+public class TwoDArray {
+
+	public static void main(String[] args) {
+		
+		// 2D array = an array of arrays
+		
+		String[][] cars = {
+								{"Camaro", "Corvette", "Silverado"},
+								{"Mustang", "Ranger", "F-150"},
+								{"Ferrari", "Lambo", "Tesla"}
+						  };
+		
+		for(int i = 0; i < cars.length; i++) {
+			System.out.println();
+			for(int j = 0; j < cars[i].length; j++) {
+				System.out.println(cars[1][j]+" ");
+			}
+		}
+		
+	}
+}
